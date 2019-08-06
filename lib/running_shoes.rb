0240939d8afdce05090849a59108a144ed77f834
Veronica Lopez <nicaa0695@gmail.com>
2019-08-06 21:43:27 +0000

    modified running_shoes.rb

--- lib/running_shoes.rb
+++ lib/running_shoes.rb
@@ -1,3 +1,6 @@
+require 'nokogiri'
+require 'pry'
+
 require_relative "running_shoes/version"
 require_relative './running_shoes/shoes'
 require_relative './running_shoes/cli'
--- lib/running_shoes/shoes.rb
+++ lib/running_shoes/shoes.rb
@@ -1,20 +1,29 @@
 class RunningShoes::Shoes
   attr_accessor :name, :price, :availability, :url
   def self.all 
-    # I should return a bunch of instances of Shoes 
-   
-    shoe_1 = self.new
-    shoe_1.name = "Duramo 9 Shoes"
-    shoe_1.price = "$65"
-    shoe_1.availability = true 
-    shoe_1.url = "https://www.adidas.com/us/women-running-shoes"
-    
-    shoe_2 = self.new
-    shoe_2.name = "Ultraboost"
-    shoe_2.price = "$180"
-    shoe_2.availability = true 
-    shoe_2.url = "https://www.adidas.com/us/women-running-shoes"
-    [shoe_1, shoe_2]
+    # Scrape Adidas and then return the shoes based on that data 
+    self.scrape_shoes
   end
   
+  def self.scrape_shoes
+    shoes = []
+    #got to adidas, finf the product
+    #extract the properties
+    #instantiate a shoe
+    
+    shoes
+  end 
+  
 end 
+
+  #     shoe_1 = self.new
+  #     shoe_1.name = "Duramo 9 Shoes"
+  #     shoe_1.price = "$65"
+  #     shoe_1.availability = true 
+  #     shoe_1.url = "https://www.adidas.com/us/women-running-shoes"
+      
+  #     shoe_2 = self.new
+  #     shoe_2.name = "Ultraboost"
+  #     shoe_2.price = "$180"
+  #     shoe_2.availability = true 
+  #     shoe_2.url = "https://www.adidas.com/us/women-running-shoes"
