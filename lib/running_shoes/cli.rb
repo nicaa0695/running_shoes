8881803e7888bf9fc43214e830dc5b085feebf3b
Veronica Lopez <nicaa0695@gmail.com>
2019-08-06 18:42:01 +0000

    modified cli.rb

--- lib/running_shoes/cli.rb
+++ lib/running_shoes/cli.rb
@@ -2,7 +2,19 @@
 class RunningShoes::CLI
   
   def call 
+    list_shoes
+    menu
+  end
+  
+  def list_shoes
     puts "Take a look at these awesome running shoes:"
-  end 
-
+    puts <<-DOC
+      1. Duramo 9 Shoes - $65 - In stock!
+      2. Ultraboost 19 Shoes - $180 - In stock!
+    DOC
+  end
+  
+  def menu
+    puts "Enter the number of the shoe you want more information about:"
+  end
 end
