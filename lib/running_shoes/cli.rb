0cecb92bdf57e02928d5390e3612895423163159
Veronica Lopez <nicaa0695@gmail.com>
2019-08-07 20:42:29 +0000

    modified shoes.rb

--- lib/running_shoes/cli.rb
+++ lib/running_shoes/cli.rb
@@ -11,7 +11,7 @@ class RunningShoes::CLI
     puts "Take a look at these awesome running shoes:"
     @shoes = RunningShoes::Shoes.all
     @shoes.each.with_index(1) do |shoe, i|
-      puts "#{i}. #{shoe.name} - #{shoe.feature} - #{shoe.availability}" 
+      puts "#{i}. #{shoe.name} - #{shoe.price} - #{shoe.availability} - #{shoe.desription}" 
     end
       
   end
@@ -23,7 +23,7 @@ class RunningShoes::CLI
       input = gets.strip.downcase
       if input.to_i > 0 
         the_shoe = @shoes[input.to_i-1]
-        puts "#{the_shoe.name} - #{the_shoe.feature} - #{the_shoe.availability}"
+        puts "#{the_shoe.name} - #{the_shoe.price} - #{the_shoe.availability} - #{the_shoe.description}"
         elsif input == "list"
           list_shoes
         else 
--- lib/running_shoes/shoes.rb
+++ lib/running_shoes/shoes.rb
@@ -1,5 +1,5 @@
 class RunningShoes::Shoes
-  attr_accessor :name, :feature, :availability
+  attr_accessor :name, :price, :availability, :description
   def self.all 
     # Scrape Altra and then return the shoes based on that data 
     self.scrape_shoes
@@ -12,14 +12,13 @@ class RunningShoes::Shoes
   end 
   
   def self.scrape_altra
-    doc = Nokogiri::HTML(open("https://www.altrarunning.com/shop/women/womens-escalante-2-alw1933g?variationId=220#hero=0"))
+    doc = Nokogiri::HTML(open("https://www.famousfootwear.com/en-US/Product/95454-1039435/adidas/Blueprt/Womens+Cloudfoam+QT+Racer+Sneaker.aspx"))
     
-    shoe = self.new 
-    shoe.name = doc.search("#product-info > h1").text
-    #price = doc.search
-    shoe.availability = "In stock!" 
-    #shoe.description = doc.search("#container-4 > div.swatches-product-details-container > section > div.pdp-details.product-details-section > div > div").text.strip
-    shoe.feature = doc.search("#container-4 > div.swatches-product-details-container > section > div.pdp-features.product-details-section.product-details-js > div").text.gsub("\n","").gsub("\t","")
+    shoe = self.new
+    shoe.name = doc.search("#ctl00_cphPageMain_BrandAndPrice1_lblDisplayName").text
+    shoe.price = doc.search("#ctl00_cphPageMain_BrandAndPrice1_ProductPrice").text.gsub("\r","").gsub("\n","").strip
+    shoe.availability = "In stock!"
+    shoe.description = doc.search("#ProductDescription").text.gsub("\r","").gsub("\n","").strip
     shoe 
     end
 end 
