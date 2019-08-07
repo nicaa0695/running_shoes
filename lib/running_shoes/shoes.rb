ff7b5fc06fdbdc3f4f5cfecc6472f89099e5c48d
Veronica Lopez <nicaa0695@gmail.com>
2019-08-07 21:42:43 +0000

    modified shoes.rb

--- lib/running_shoes/shoes.rb
+++ lib/running_shoes/shoes.rb
@@ -9,6 +9,10 @@ class RunningShoes::Shoes
     shoes = []
     shoes << self.scrape_altra
     shoes << self.scrape_famous
+    shoes << self.scrape_puma
+    shoes << self.scrape_armour
+    shoes << self.scrape_balance
+    shoes << self.scrape_asics
     shoes
   end 
   
@@ -33,16 +37,49 @@ class RunningShoes::Shoes
       shoe.description = doc.search("#ProductDescription").text.gsub("\r","").gsub("\n","").strip
       shoe 
     end
+    
+    def self.scrape_puma 
+      doc = Nokogiri::HTML(open("https://www.famousfootwear.com/en-US/Product/95275-1047154/Puma/Black_White/Womens+Weave+Running+Shoe.aspx"))
+      
+      shoe = self.new 
+      shoe.name = doc.search("#ctl00_cphPageMain_BrandAndPrice1_lblDisplayName").text
+      shoe.price = doc.search("#ctl00_cphPageMain_BrandAndPrice1_ProductPrice").text.gsub("\r","").gsub("\n","").gsub(/\s+/,"")
+      shoe.availability = "In stock!"
+      shoe.description = doc.search("#ProductDescription").text.gsub("\r","").gsub("\n","").strip
+      shoe 
+    end
+    
+    def self.scrape_armour
+      doc = Nokogiri::HTML(open("https://www.famousfootwear.com/en-US/Product/94957-1047132/Under+Armour/Black_Black/Womens+Charged+Impulse+Running+Shoe.aspx"))
+      
+      shoe = self.new 
+      shoe.name = doc.search("#ctl00_cphPageMain_BrandAndPrice1_lblDisplayName").text
+      shoe.price = doc.search("#ctl00_cphPageMain_BrandAndPrice1_ProductPrice").text.gsub("\r","").gsub("\n","").gsub(/\s+/,"")
+      shoe.availability = "In stock!"
+      shoe.description = doc.search("#ProductDescription").text.gsub("\r","").gsub("\n","").strip
+      shoe 
+    end
+      
+    def self.scrape_balance 
+      doc = Nokogiri::HTML(open("https://www.famousfootwear.com/en-US/Product/95366-1042457/New+Balance/Navy/Womens+Nergize+Sneaker.aspx"))
+      
+      shoe = self.new 
+      shoe.name = doc.search("#ctl00_cphPageMain_BrandAndPrice1_lblDisplayName").text
+      shoe.price = doc.search("#ctl00_cphPageMain_BrandAndPrice1_ProductPrice").text.gsub("\r","").gsub("\n","").gsub(/\s+/,"")
+      shoe.availability = "In stock!"
+      shoe.description = doc.search("#ProductDescription").text.gsub("\r","").gsub("\n","").strip
+      shoe 
+    end
+    
+    def self.scrape_asics 
+      doc = Nokogiri::HTML(open("https://www.famousfootwear.com/en-US/Product/94475-1037179/ASICS/Black_Laser+Pink/Womens+GEL-Quantum+180+2+Running+Shoe.aspx"))
+      
+      shoe = self.new 
+      shoe.name = doc.search("#ctl00_cphPageMain_BrandAndPrice1_lblDisplayName").text
+      shoe.price = doc.search("#ctl00_cphPageMain_BrandAndPrice1_ProductPrice").text.gsub("\r","").gsub("\n","").gsub(/\s+/,"")
+      shoe.availability = "In stock!"
+      shoe.description = doc.search("#ProductDescription").text.gsub("\r","").gsub("\n","").strip
+      shoe 
+    end 
 end 
 
-  #     shoe_1 = self.new
-  #     shoe_1.name = "Duramo 9 Shoes"
-  #     shoe_1.price = "$65"
-  #     shoe_1.availability = true 
-  #     shoe_1.url = "https://www.adidas.com/us/women-running-shoes"
-      
-  #     shoe_2 = self.new
-  #     shoe_2.name = "Ultraboost"
-  #     shoe_2.price = "$180"
-  #     shoe_2.availability = true 
-  #     shoe_2.url = "https://www.adidas.com/us/women-running-shoes"
