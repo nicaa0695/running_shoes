4c83151b5723a35d63bcbeb5fb794a28cefb796d
Veronica Lopez <nicaa0695@gmail.com>
2019-08-07 21:25:19 +0000

    modified shoes.rb

--- lib/running_shoes/shoes.rb
+++ lib/running_shoes/shoes.rb
@@ -8,6 +8,7 @@ class RunningShoes::Shoes
   def self.scrape_shoes
     shoes = []
     shoes << self.scrape_altra
+    shoes << self.scrape_famous
     shoes
   end 
   
@@ -26,8 +27,8 @@ class RunningShoes::Shoes
       doc = Nokogiri::HTML(open("https://www.famousfootwear.com/en-US/Product/93921-1046863/Nike/Black_White/Womens+Acalme+Sneaker.aspx"))
       
       shoe = self.new 
-      name = doc.search("#ctl00_cphPageMain_BrandAndPrice1_lblDisplayName").text
-      price = doc.search("#ctl00_cphPageMain_BrandAndPrice1_ProductPrice").text.gsub("\r","").gsub("\n","").gsub(/\s+/,"")
+      shoe.name = doc.search("#ctl00_cphPageMain_BrandAndPrice1_lblDisplayName").text
+      shoe.price = doc.search("#ctl00_cphPageMain_BrandAndPrice1_ProductPrice").text.gsub("\r","").gsub("\n","").gsub(/\s+/,"")
       shoe.availability = "In stock!"
       shoe.description = doc.search("#ProductDescription").text.gsub("\r","").gsub("\n","").strip
       shoe 
