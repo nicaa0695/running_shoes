e1aaba0ea36837e7843232198260a93e8e088c2f
Veronica Lopez <nicaa0695@gmail.com>
2019-08-07 21:21:12 +0000

    mofified shoes.rb

--- lib/running_shoes/shoes.rb
+++ lib/running_shoes/shoes.rb
@@ -16,10 +16,21 @@ class RunningShoes::Shoes
     
     shoe = self.new
     shoe.name = doc.search("#ctl00_cphPageMain_BrandAndPrice1_lblDisplayName").text
-    shoe.price = doc.search("#ctl00_cphPageMain_BrandAndPrice1_ProductPrice").text.gsub("\r","").gsub("\n","").strip
+    shoe.price = doc.search("#ctl00_cphPageMain_BrandAndPrice1_ProductPrice").text.gsub("\r","").gsub("\n","").gsub(/\s+/,"")
     shoe.availability = "In stock!"
     shoe.description = doc.search("#ProductDescription").text.gsub("\r","").gsub("\n","").strip
     shoe 
+    end
+    
+    def self.scrape_famous
+      doc = Nokogiri::HTML(open("https://www.famousfootwear.com/en-US/Product/93921-1046863/Nike/Black_White/Womens+Acalme+Sneaker.aspx"))
+      
+      shoe = self.new 
+      name = doc.search("#ctl00_cphPageMain_BrandAndPrice1_lblDisplayName").text
+      price = doc.search("#ctl00_cphPageMain_BrandAndPrice1_ProductPrice").text.gsub("\r","").gsub("\n","").gsub(/\s+/,"")
+      shoe.availability = "In stock!"
+      shoe.description = doc.search("#ProductDescription").text.gsub("\r","").gsub("\n","").strip
+      shoe 
     end
 end 
 
