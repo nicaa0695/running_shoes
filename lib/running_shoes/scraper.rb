21fda962ca0f09fbeebc108e1ec9921a8dd41721
Veronica Lopez <nicaa0695@gmail.com>
2019-08-09 21:23:57 +0000

    modified scraper.rb

--- lib/running_shoes/scraper.rb
+++ lib/running_shoes/scraper.rb
@@ -1,27 +1,6 @@
-# class RunningShoes::Scraper
-#   def scrape
-#     doc = Nokogiri::HTML(open(self.url))
-#       self.availability = "In stock!"
-#       self.description = doc.search("#ProductDescription").text.gsub("\r","").gsub("\n","").strip
-#       self
-#   end
+class RunningShoes::Scraper
   
-#   def self.all 
-#     self.scrape_all if @@all.empty?
-#     @@all
-#     #self.scrape_shoes
-#   end
   
-#   def self.scrape_all
-#     doc = Nokogiri::HTML(open("https://www.famousfootwear.com/en-US/Womens/_/_/Athletic+Shoes~Running+Shoes~Lifestyle/_/Products.aspx"))
-#     shoe_cards = doc.css('.productCellWrapper')
-#     shoe_cards.each do |shoe_card|
-#       shoe = self.new
-#       shoe.name = shoe_card.css('span.styleName.name').text
-#       shoe.price = shoe_card.css('span.variantPrice').text 
-#       shoe.url = "https://www.famousfootwear.com/#{shoe_card.css('a').attr('href').text}"
-#     end
-#   end
-# end 
+end 
   
   
